29143c8ce2331af3a2a50270e23ac00d3496afa4
GregoryGeslinB <gregorypro31@gmail.com>
2023-10-14 21:22:33 +0200

    note sur 20

--- Appréciation de note.py
+++ Appréciation de note.py
@@ -1,13 +1,13 @@
 # Premiere version 
 
-for i in range(1,11):
+for i in range(1,21):
     print (i)
     i += 1
-    if i <=5 :
+    if i <=10 :
         print ("La note est faible")
-    elif i == 6: 
+    elif i == 11: 
         print ("La note est moyen")
-    elif i == 11 :
+    elif i == 21 :
         print ("La note est maximal")
     else :
         print ("La note est supérieur à la moyenne")
